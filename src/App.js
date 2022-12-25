7a645fb54d0d529e82267acaceb69f8e9b9ea078
Puja Dey <pujadey48@gmail.com>
2022-12-25 21:34:32 +0600

    1st commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <button className="btn btn-info">Info</button>
+      <button className="btn btn-success">Success</button>
+      <button className="btn btn-warning">Warning</button>
+      <button className="btn btn-error">Error</button>
+    </div>
+  );
+}
+
+export default App;
